02ffbc858ca828b2dcdec14f1ede2ae5b37471a5
jpataflafla <jakub.kozlowski.mail@gmail.com>
2023-11-20 17:44:06 +0100

    Create bubble_sort.js

---
+++ Algorithms/2_Sorting/javascript/bubble_sort.js
@@ -0,0 +1,38 @@
+//Bubble sort
+
+const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
+
+function bubbleSort(array) {
+  for (let i = 0; i < array.length - 1; i++) {
+    for (let j = 0; j < array.length - 1; j++) {
+      if (array[j] > array[j + 1]) {
+        // Swap numbers
+        let temp = array[j];
+        array[j] = array[j + 1];
+        array[j + 1] = temp;
+      }
+    }
+  }
+}
+
+const bubbleSort2 = (arr) => {
+  let swapped;
+
+  do {
+    swapped = false;
+    for (let i = 0; i < arr.length - 1; i++) {
+      if (arr[i] > arr[i + 1]) {
+        let temp = arr[i];
+        arr[i] = arr[i + 1];
+        arr[i + 1] = temp;
+        swapped = true;
+      }
+    }
+  } while (swapped);
+
+  return arr;
+};
+
+//bubbleSort(numbers);
+bubbleSort2(numbers);
+console.log(numbers);
